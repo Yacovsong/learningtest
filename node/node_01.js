d5caad610fdeef8da11a1e38d1185da874202ffd
YacovSong <499071541@qq.com>
2018-04-16 23:40:18 +0800

    app1

---
+++ node/node_01.js
@@ -0,0 +1,17 @@
+/**
+ * Created by YacovSong on 2018/4/14.
+ */
+var aa=function () {
+    console.log(4);
+}
+function aa() {
+    console.log(51);
+}
+
+aa();
+
+//作用域 预编译 v8
+// 1：预编译 var function
+// 2：按照文档顺序执行
+
+
---
+++ node/testnode11.js
@@ -0,0 +1,4 @@
+/**
+ * Created by YacovSong on 2018/4/15.
+ */
+//node get
